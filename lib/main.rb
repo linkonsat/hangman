bd428ab04516793f715f10fcab4f735f9f76e1a9
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-09-22 12:13:43 -0700

    Created files for game objects in ruby

--- lib/main.rb
+++ lib/main.rb
@@ -1,12 +1,15 @@
 require 'pry-byebug'
 require 'yaml'
-require './scripts/test'
+require './scripts/game.rb'
 
 bark()
 def game_start
-
+ 
+    
 end
 
 def check_save_file
-
+puts "found a saved game! would you like to continue it?(test)"
 end
+
+check_save_file()
